781c67bba2c423573aa311029527bc03bb1ab496
acharpentier2018 <acharpentier2018@cchsraider.net>
2017-09-21 13:52:53 -0400

    first commit

---
+++ Program2/src/Problem2.java
@@ -0,0 +1,18 @@
+/*
+ *Alex Charpentier
+ *Problem2
+ * 
+ */
+public class Problem2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("A well-formed Java program has");
+		System.out.println("a main method with { and }");
+		System.out.println("braces.");
+		
+		System.out.println("\nA System.out.println statement");
+		System.out.println("has ( and ) and usually a"); 
+	}
+
+}
