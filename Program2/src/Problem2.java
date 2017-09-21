fbf09844df15fc143ac878bbe66869fd43aedfc7
acharpentier2018 <acharpentier2018@cchsraider.net>
2017-09-21 14:03:13 -0400

    3rd commit

--- Program2/src/Problem2.java
+++ Program2/src/Problem2.java
@@ -1,18 +1,20 @@
 /*
  *Alex Charpentier
  *Problem2
- * 
+ * Prints strings
  */
 public class Problem2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//prints the first 3 line
 		System.out.println("A well-formed Java program has");
 		System.out.println("a main method with { and }");
 		System.out.println("braces.");
 		
+		//prints the last 3 lines
 		System.out.println("\nA System.out.println statement");
-		System.out.println("has ( and ) and usually a"); 
+		System.out.println("has ( and ) and usually a");
+		System.out.println("String enclosed between \" and \"");
 	}
 
 }
