de25c634d2846af491db05ee7429acdf5c16b56a
Alex <Alex@10.0.0.72>
2017-09-24 15:56:27 -0400

    Commit

---
+++ Program1/src/Problem1.java
@@ -0,0 +1,15 @@
+/*
+ * Alex Charpentier
+ * Problem1
+ * Prints a string
+ */
+public class Problem1 {
+
+	public static void main(String[] args) {
+		//Prints to console "Victory is mine!" including / and |
+		System.out.println("//////////////////////");
+		System.out.println("|| Victory is mine! ||");
+		System.out.println("//////////////////////");
+	}
+
+}
